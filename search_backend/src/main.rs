2655f77e4a6a26ff95f227a44adc8b63d094ebbc
Nishanth Salinamakki <nsalina@protonmail.com>
2023-11-29 02:08:39 -0500

    Cleaned up some code

--- search_backend/src/main.rs
+++ search_backend/src/main.rs
@@ -10,33 +10,13 @@ struct SearchParams {
     query: String,
 }
 
-#[derive(Serialize, Deserialize)]
-struct TypesenseSearchParams {
-    q: String,
-    query_by: String,
-    sort_by: String,
-    page: u32,
-    per_page: u32,
-}
-
 async fn search_typesense_idx(query: String) {
     let client = reqwest::Client::new();
     let mut headers = HeaderMap::new();
     headers.insert(CONTENT_TYPE, "application/json".parse().unwrap());
     headers.insert("X-TYPESENSE-API-KEY", "xyz".parse().unwrap());
     let typesense_host = Url::parse("http://127.0.0.1:8108").unwrap();
-    println!("Typesense host: {}", typesense_host);
     let collection_name = "educational_video_transcriptions";
-    let search_query_json = json!(
-        {"q": query, "query_by": "content", "sort_by": "start_time:asc", "page": 1, "per_page": 25}
-    );
-    // let search_query = TypesenseSearchParams {
-    //     q: query,
-    //     query_by: "content".to_string(),
-    //     sort_by: "start_time:asc".to_string(),
-    //     page: 1,
-    //     per_page: 25,
-    // };
     let search_query_params = [
         ("q", query.as_str()),
         ("query_by", "content"),
@@ -44,8 +24,7 @@ async fn search_typesense_idx(query: String) {
         ("page", "1"),
         ("per_page", "25"),
     ];
-    println!("Search request: {:?}", search_query_json);
-    println!("Searching with: {}", query);
+    println!("Search request: {:?}", search_query_params);
     let search_response = client.get(format!("{}/collections/{}/documents/search", typesense_host, collection_name))
         .headers(headers)
         .query(&search_query_params)
@@ -53,8 +32,8 @@ async fn search_typesense_idx(query: String) {
         .await
         .expect(format!("Failed to get search response from Typesense idx for query: {}", query).as_str());
     println!("Search response status: {}", search_response.status());
-    // let search_response_json = search_response.json().await.expect("Failed to parse search response as JSON.");
     println!("{:?}", search_response.text().await.unwrap());
+    // TODO: Deserialize response into serde struct and return that to user in handler fn
 }
 
 async fn handler(Query(search_params): Query<SearchParams>) -> String {
