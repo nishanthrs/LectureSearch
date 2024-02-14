245e1739903130472bdfca6aa5c7895cc0b35793
nishanthrs <nsalina@protonmail.com>
2024-02-13 21:38:31 -0500

    Resolved minor errors with code and typesense config

--- search_backend/src/main.rs
+++ search_backend/src/main.rs
@@ -1,12 +1,14 @@
-use anyhow::{Result};
+use anyhow::Result;
 use axum::{extract::Query, http::StatusCode, routing::get, Json, Router};
-use serde::{Serialize, Deserialize};
 use serde::de::value::MapDeserializer;
+use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 use typesense_codegen::apis::configuration::{ApiKey, Configuration};
 use typesense_codegen::apis::documents_api::search_collection;
-use typesense_codegen::models::{SearchParameters};
+use typesense_codegen::models::SearchParameters;
 use url::Url;
+
+const TYPESENSE_API_KEY: &str = "ncN2n85vYxgCg45khosRNlOb0vEu6gyEYB396h2zelSMZzyg";
 
 #[derive(Deserialize)]
 struct SearchParams {
@@ -19,10 +21,10 @@ struct SearchReqParams {
     query_by: String,
     sort_by: String,
     page: i32,
-    per_page: i32
+    per_page: i32,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct VideoTranscriptionDoc {
     channel: String,
     channel_follower_count: i32,
@@ -34,17 +36,23 @@ struct VideoTranscriptionDoc {
     title: String,
     upload_date: String,
     video_id: String,
-    view_count: i32
+    view_count: i32,
 }
 
-async fn search_typesense_idx(query: String) -> Result<Vec<VideoTranscriptionDoc>>  {
+async fn search_typesense_idx(query: String) -> Result<Vec<VideoTranscriptionDoc>> {
     let client = reqwest::Client::new();
-    let typesense_host = Url::parse("http://127.0.0.1:8108").unwrap();
+    let typesense_host = Url::parse("http://0.0.0.0:8108").unwrap();
+    /* TODO: Will not work unless collection is initialized for this new Ubuntu Typesense server!
+    Run indexing script from AI_playground again to get this working.
+    Or just starting working on deploying it as a service right away! */
     let collection_name = "educational_video_transcriptions";
     let typesense_config = Configuration {
         base_path: typesense_host.to_string(),
         client,
-        api_key: Some(ApiKey {prefix: None, key: "xyz".to_string()}),
+        api_key: Some(ApiKey {
+            prefix: None,
+            key: TYPESENSE_API_KEY.to_string(),
+        }),
         basic_auth: None,
         oauth_access_token: None,
         bearer_access_token: None,
@@ -59,11 +67,8 @@ async fn search_typesense_idx(query: String) -> Result<Vec<VideoTranscriptionDoc
         ..Default::default()
     };
 
-    let search_response = search_collection(
-        &typesense_config,
-        collection_name,
-        typesense_search_params,
-    ).await?;
+    let search_response =
+        search_collection(&typesense_config, collection_name, typesense_search_params).await?;
     match search_response.hits {
         Some(hits) => {
             println!("Got {} hits", hits.len());
@@ -76,22 +81,26 @@ async fn search_typesense_idx(query: String) -> Result<Vec<VideoTranscriptionDoc
                     Some(doc) => Some(
                         // TODO: Look into better ways of deserializing HashMap<String, serde_json::Value> into VideoTranscriptionDoc
                         // https://www.google.com/search?q=convert+hashmap+to+serde_json%3A%3AMap
-                        VideoTranscriptionDoc::deserialize(
-                            MapDeserializer::new(doc.clone().into_iter())
-                        ).unwrap()
+                        VideoTranscriptionDoc::deserialize(MapDeserializer::new(
+                            doc.clone().into_iter(),
+                        ))
+                        .unwrap(),
                     ),
                     // Should not happen due to skip_serializing_if macro in SearchResultHit struct
                     None => None,
                 })
                 .collect();
             Ok(video_docs)
-        },
+        }
         None => Ok(vec![]),
     }
 }
 
-async fn handler(Query(search_params): Query<SearchParams>) -> (StatusCode, Json<Vec<VideoTranscriptionDoc>>) {
+async fn handler(
+    Query(search_params): Query<SearchParams>,
+) -> (StatusCode, Json<Vec<VideoTranscriptionDoc>>) {
     let video_docs = search_typesense_idx(search_params.query).await.unwrap();
+    println!("Video docs: {:?}", video_docs);
     (StatusCode::OK, Json(video_docs))
 }
 
